679279a8cdafd9630aa2bf73064618b1b697e482
atinachang <atinachang@gmail.com>
2021-03-13 12:15:22 -0500

    basic call for heroku testing

--- server.js
+++ server.js
@@ -7,25 +7,29 @@ const routes = require('./routes/data');
 dotenv.config({ path: './config.env' });
 const app = express();
 
-mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true,
+app.get('/', (req, res) => {
+        res.send({ hi: 'there' });
 });
 
-mongoose.connection.on('connected', () => {
-        console.log('MongoDB connected');
-});
+// mongoose.connect(process.env.MONGO_URI, {
+//         useNewUrlParser: true,
+//         useCreateIndex: true,
+//         useFindAndModify: false,
+//         useUnifiedTopology: true,
+// });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ extended: false }));
+// mongoose.connection.on('connected', () => {
+//         console.log('MongoDB connected');
+// });
 
-app.use('/api/locaid', routes);
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json({ extended: false }));
 
-if (process.env.NODE_ENV === 'production') {
-        app.use(express.static('client/build'));
-}
+// app.use('/api/locaid', routes);
+
+// if (process.env.NODE_ENV === 'production') {
+//         app.use(express.static('client/build'));
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Listening on port ${PORT} 🐙`));
